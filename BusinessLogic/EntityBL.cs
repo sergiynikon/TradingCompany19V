b267861678a3e57d75f29972e054e4c02d4f6a5e
Sergiy Nikon <sergiy.nikon2014@gmail.com>
2019-01-02 21:47:06 +0200

    added BusinessLogic project

---
+++ BusinessLogic/EntityBL.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using BusinessLogic.Interfaces;
+using DAL;
+using DTO;
+
+namespace BusinessLogic
+{
+    public class EntityBL : IEntityBL
+    {
+        private UserDAL _userDAL;
+        private SupplierDAL _supplierDAL;
+        private OrderDAL _orderDAL;
+
+        public int AddOrder(OrderDTO order)
+        {
+            return _orderDAL.AddOrder(order);
+        }
+
+        public int AddSupplier(SupplierDTO supplier)
+        {
+            return _supplierDAL.AddSupplier(supplier);
+        }
+
+        public int AddUser(UserDTO user)
+        {
+            return _userDAL.AddUser(user);
+        }
+
+        public int DeleteOrderByID(int id)
+        {
+            return _orderDAL.DeleteOrderByID(id);
+        }
+
+        public int DeleteSupplierByID(int id)
+        {
+            return _supplierDAL.DeleteSupplierByID(id);
+        }
+
+        public List<OrderDTO> GetAllOrders()
+        {
+            return _orderDAL.GetAllOrders();
+        }
+
+        public List<SupplierDTO> GetAllSuppliers()
+        {
+            return _supplierDAL.GetAllSuppliers();
+        }
+
+        public OrderDTO GetOrderByID(int id)
+        {
+            return _orderDAL.GetOrderByID(id);
+        }
+
+        public SupplierDTO GetSupplierByID(int id)
+        {
+            return _supplierDAL.GetSupplierByID(id);
+        }
+
+        public void Update(OrderDTO order)
+        {
+            _orderDAL.Update(order);
+        }
+
+        public UserDTO ValidateLogin(string login, string password)
+        {
+            return _userDAL.ValidateLogin(login, password);
+        }
+    }
+}
---
+++ BusinessLogic/Interfaces/IEntityBL.cs
@@ -0,0 +1,20 @@
+﻿using DTO;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IEntityBL
+    {
+        OrderDTO GetOrderByID(int id);
+        List<OrderDTO> GetAllOrders();
+        int AddOrder(OrderDTO order);
+        int DeleteOrderByID(int id);
+        void Update(OrderDTO order);
+        int AddSupplier(SupplierDTO supplier);
+        int DeleteSupplierByID(int id);
+        SupplierDTO GetSupplierByID(int id);
+        List<SupplierDTO> GetAllSuppliers();
+        int AddUser(UserDTO user);
+        UserDTO ValidateLogin(string login, string password);
+    }
+}
